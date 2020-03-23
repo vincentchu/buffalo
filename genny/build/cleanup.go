97c444d4b86409486ca927407e7bf491c816d07d
Stanislas Michalak <stanislas.michalak@gmail.com>
2020-03-23 21:15:16 +0100

    More logs

--- genny/build/cleanup.go
+++ genny/build/cleanup.go
@@ -15,6 +15,7 @@ func Cleanup(opts *Options) genny.RunFn {
 	return func(r *genny.Runner) error {
 		defer os.RemoveAll(filepath.Join(opts.Root, "a"))
 		if err := jam.Clean(); err != nil {
+			r.Logger.Error("could not jam.Clean")
 			return err
 		}
 
